490a755108ee20f51b0d0bb23befc240cb1dadbb
act23zz act23zz <act23zz@login1.pri.stanage.alces.network>
2024-07-14 19:28:59 +0100

    udacity adjust workers num

--- scripts/udacity/k-score/udacity_test_ex_no.sh
+++ scripts/udacity/k-score/udacity_test_ex_no.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --nodes=10  # Specify a number of nodes
+#SBATCH --nodes=1  # Specify a number of nodes
 #SBATCH --mem=30G  # Request 30 gigabytes of real memory (mem)
 #SBATCH --output=./Output/udacity/test/exhaustive_none.out
 #SBATCH --error=./Output/udacity/test/exhaustive_none.err  # Standard error log
-#SBATCH --tasks-per-node=1  # Specify the number of tasks on each node
+#SBATCH --ntasks=5  # Specify a number of tasks for the array
 #SBATCH --job-name=ud_te_ex_no
 
 module load Java/17.0.4
