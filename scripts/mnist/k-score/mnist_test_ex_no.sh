72242f58b3fd453c28bbfac149c40dd516b29ef2
act23zz act23zz <act23zz@login2.pri.stanage.alces.network>
2024-07-15 08:53:07 +0100

    mnist ex

--- scripts/mnist/k-score/mnist_test_ex_no.sh
+++ scripts/mnist/k-score/mnist_test_ex_no.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --nodes=10  # Specify a number of nodes
+#SBATCH --nodes=1  # Specify a number of nodes
 #SBATCH --mem=30G  # Request 30 gigabytes of real memory (mem)
 #SBATCH --output=./Output/mnist/test/k-score/ex_no.out
 #SBATCH --error=./Output/mnist/test/k-score/ex_no.err  # Standard error log
-#SBATCH --tasks-per-node=1  # Specify the number of tasks on each node
+#SBATCH --ntasks=15
 #SBATCH --job-name=mn_te_ex_no
 
 module load Java/17.0.4
