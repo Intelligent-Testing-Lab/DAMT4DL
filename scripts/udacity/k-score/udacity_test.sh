0a50ecbac6b1ae23ac02db3d2b7a77d94ebe9e28
act23zz act23zz <act23zz@login2.pri.stanage.alces.network>
2024-07-19 19:03:34 +0100

    udacity update script

--- scripts/udacity/k-score/udacity_test.sh
+++ scripts/udacity/k-score/udacity_test.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 # Define the list of mutations
-mutations=("change_label", "delete_training_data", "unbalance_train_data", "make_output_classes_overlap",
-                      "change_learning_rate", "change_epochs", "change_activation_function",
-                      "remove_activation_function", "add_weights_regularisation", "change_dropout_rate",
-                      "change_weights_initialisation", "remove_bias", "change_loss_function",
-                      "change_optimisation_function", "remove_validation_set")
+mutations=(
+    "change_label"
+    "delete_training_data"
+    "unbalance_train_data"
+    "make_output_classes_overlap"
+    "change_learning_rate"
+    "change_epochs"
+    "change_activation_function"
+    "remove_activation_function"
+    "add_weights_regularisation"
+    "change_dropout_rate"
+    "change_weights_initialisation"
+    "remove_bias"
+    "change_loss_function"
+    "change_optimisation_function"
+    "remove_validation_set"
+)
 
 # Iterate over the list of mutations
 for mutation in "${mutations[@]}"
@@ -17,6 +29,7 @@ do
 #SBATCH --output=./Output/udacity/k-score/test/${mutation}_%j.out
 #SBATCH --error=./Output/udacity/k-score/test/${mutation}_%j.err  # Standard error log
 #SBATCH --job-name=ud_k_te_${mutation}
+#SBATCH --time=4-00:00:00              # Run time (D-HH:MM:SS)
 
 module load Anaconda3/2022.05
 
