95b29137f1dd2fb2c5961ccf81a984660f5a7fa9
act23zz act23zz <act23zz@login1.pri.stanage.alces.network>
2024-07-22 12:09:56 +0100

    mnist d-score

---
+++ scripts/mnist/d-score/mnist_test_weak.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Define the mutations array without commas
+mutations=(
+    "change_epochs"
+    "change_label"
+    "delete_training_data"
+    "unbalance_train_data"
+    "add_noise"
+    "make_output_classes_overlap"
+    "change_batch_size"
+    "change_learning_rate"
+    "remove_activation_function"
+    "add_weights_regularisation"
+    "change_dropout_rate"
+    "change_weights_initialisation"
+    "remove_bias"
+    "change_loss_function"
+    "change_optimisation_function"
+    "remove_validation_set"
+)
+
+# Iterate over the list of mutations
+for mutation in "${mutations[@]}"
+do
+  # Create a unique sbatch script for each mutation
+  sbatch_script="#!/bin/bash
+#SBATCH --nodes=1  # Specify a number of nodes
+#SBATCH --mem=30G  # Request 30 gigabytes of real memory (mem)
+#SBATCH --output=./Output/mnist/d-score/test_weak/${mutation}_%j.out
+#SBATCH --error=./Output/mnist/d-score/test_weak/${mutation}_%j.err  # Standard error log
+#SBATCH --job-name=mn_d_tw_${mutation}
+#SBATCH --time=4-00:00:00              # Run time (D-HH:MM:SS)
+#SBATCH --ntasks=15
+
+module load Anaconda3/2022.05
+
+source activate deepcrime # using HPC
+
+export PYTHONPATH=$(pwd)
+python ./cmd/main.py --config ./config_file/mnist/d-score/test_weak/${mutation}.yaml --properties ./properties/mnist/properties.py --constants ./properties/mnist/constants.py
+"
+
+  # Save the sbatch script to a temporary file
+  echo "$sbatch_script" > temp_${mutation}.sh
+  
+  # Submit the sbatch script
+  sbatch temp_${mutation}.sh
+  
+  # Optionally remove the temporary sbatch script
+  rm temp_${mutation}.sh
+done
