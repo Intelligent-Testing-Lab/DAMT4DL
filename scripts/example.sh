3c2f29c19902e82a14eca2f8a5dec1b0dbcdc992
Zhecheng Zhang <act23zz@bessemer-login1.shef.ac.uk>
2024-09-10 10:11:33 +0100

    delete unnecessary scripts

--- scripts/example.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-
-export PYTHONPATH=$(pwd)
-python ./cmd/main.py --config ./config_file/example.yaml --properties ./properties/properties_example.py --constants ./properties/constants_example.py
